e8e3de5969d111d02a94511ae5ea96c7f5697fa3
George Leyva <leyvageorge@hotmail.com>
2022-11-16 19:24:27 -0800

    Changes to page

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
+let header = document.querySelector('#page-header')
+header.style.textAlign = "left"
 
-// Select some elements...
+let dogImages = document.querySelectorAll('.dog-image')
+for(let i = 0; i < dogImages.length; i++){
+	dogImages[i].style.borderRadius = '50px'
+}
+
+//moving name of dog to be left align
+let dogName = document.querySelectorAll('.dog-name')
+for(let i = 0; i < dogName.length; i++){
+    dogName[i].style.textAlign = "left"
+}
+
+//change the color to footer text
+let footerColor = document.querySelector('.footer')
+footerColor.style.color = 'red'
+
+//fliping the image 180 Degrees
+for(let i = 0; i < dogImages.length; i++){
+	dogImages[i].style.trasform = 'rotate(180deg)'
+}
